2efc8168cab4067c931ae395f3a6c09caa9f102c
Sheldon Codling <codlinsh@gmail.com>
2023-02-13 23:18:10 -0500

    fixed port issue

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def handle_end():
     return "ok"
 
 if __name__ == "__main__":
-    host = "0.0.0.0"
+    host = "0.0.0.0:$PORT"
     #port = int(os.environ.get("PORT", "8080"))
 
     # print(f"\nRunning Battlesnake server at http://{host}:{port}")
